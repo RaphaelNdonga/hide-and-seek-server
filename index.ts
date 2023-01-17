4194add3acc14fd197d4ce276c41fcf3eb20bf71
RaphaelNdonga <nkibi53@gmail.com>
2023-01-17 17:04:13 +0300

    port changed

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 8082 });
 const state: Player[] = []
 
 wss.on('connection', function connection(ws) {
@@ -8,7 +8,6 @@ wss.on('connection', function connection(ws) {
     const json = JSON.parse(data.toString()) as Player
     console.log('received: %s', json);
     const doesItExist = state.find(a => a.ethAddress == json.ethAddress)
-    console.log(doesItExist);
     
     if(doesItExist) doesItExist.position = json.position
     else state.push(json)
